fb12ee05c6f9df7cac03e2844719d710ed932571
Darkegn <erdemegin34@gmail.com>
2024-05-08 03:28:25 +0300

    update

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -1,30 +1,25 @@
-# -*- coding: utf-8 -*-
 import scrapy
 
 
-class BooksSpider(scrapy.Spider):
+class GarantiCampaignSpider(scrapy.Spider):
     name = "books"
-    allowed_domains = ["books.toscrape.com"]
+    allowed_domains = ["garantibbva.com.tr"]
     start_urls = [
-        'http://books.toscrape.com/',
+        'https://www.garantibbva.com.tr/kampanyalar',
     ]
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
+        for promotion_url in response.css("div.promotion-list-item a.promotion-detail ::attr(href)").extract():
+            yield scrapy.Request(response.urljoin(promotion_url), callback=self.parse_promotion_page)
+        next_page = response.css("a.pagination-next ::attr(href)").extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
-    def parse_book_page(self, response):
+    def parse_promotion_page(self, response):
         item = {}
-        product = response.css("div.product_main")
-        item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
+        item["title"] = response.css("h1.promotion-detail-header ::text").extract_first()
+        item['category'] = response.css("span.promotion-category ::text").extract_first()
+        item['description'] = response.css("div.promotion-detail-text ::text").extract_first()
+        item['validity'] = response.css("div.promotion-detail-text > p:nth-of-type(1) strong ::text").extract_first()
+        item['conditions'] = "\n".join(response.css("div.promotion-detail-text ul > li ::text").extract())
         yield item
