d68e4330c5783754a597b882c1c6e9ce48284bc0
Darkegn <erdemegin34@gmail.com>
2024-05-08 03:32:55 +0300

    update

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -1,25 +1,22 @@
 import scrapy
 
-
 class GarantiCampaignSpider(scrapy.Spider):
-    name = "books"
+    name = "garanti_campaigns"
     allowed_domains = ["garantibbva.com.tr"]
     start_urls = [
         'https://www.garantibbva.com.tr/kampanyalar',
     ]
 
     def parse(self, response):
-        for promotion_url in response.css("div.promotion-list-item a.promotion-detail ::attr(href)").extract():
+        for promotion_url in response.css("div.card__griditem a ::attr(href)").extract():
             yield scrapy.Request(response.urljoin(promotion_url), callback=self.parse_promotion_page)
-        next_page = response.css("a.pagination-next ::attr(href)").extract_first()
+        next_page = response.css("a.pagination__next ::attr(href)").extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
     def parse_promotion_page(self, response):
         item = {}
-        item["title"] = response.css("h1.promotion-detail-header ::text").extract_first()
-        item['category'] = response.css("span.promotion-category ::text").extract_first()
-        item['description'] = response.css("div.promotion-detail-text ::text").extract_first()
-        item['validity'] = response.css("div.promotion-detail-text > p:nth-of-type(1) strong ::text").extract_first()
-        item['conditions'] = "\n".join(response.css("div.promotion-detail-text ul > li ::text").extract())
+        item["title"] = response.css("div.card-title ::text").extract_first()
+        item['image_url'] = response.css("img.lozad ::attr(data-src)").extract_first()
+        item['description'] = response.css("div.card-content ::text").extract_first()
         yield item
